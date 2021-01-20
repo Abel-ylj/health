6b74af8e0b3b7d98ae4c1232d85ecaed339c9075
ylj <aboutlistener@163.com>
2021-01-20 14:28:02 +0800

    H5工程: 页面，工具类，maven坐标(重点是H5工程也是消费dubbo服务的，也需要依赖interface接口工程)

--- parent/common/src/main/java/cn/ylj/constant/RedisConstant.java
+++ parent/common/src/main/java/cn/ylj/constant/RedisConstant.java
@@ -7,4 +7,9 @@ public class RedisConstant {
 
     //套餐图片保存在数据库中的图片名称(被套餐实际引用的图片集合)
     public static final String SETMEAL_PIC_DB_RESOURCES = "health:setmealPicDbResources";
+
+    //验证码
+    public static final String SENDTYPE_ORDER = "health:order001:";//用于缓存体检预约时发送的验证码
+    public static final String SENDTYPE_LOGIN = "health:login002";//用于缓存手机号快速登录时发送的验证码
+    public static final String SENDTYPE_GETPWD = "health:forgetpwd003";//用于缓存找回密码时发送的验证码
 }
